e98083d2ef22de0de766ce9ed185ef11937cf9a8
larukedi <eser@sent.com>
2014-06-17 03:51:35 +0300

    * author page.

---
+++ BulletinReader/Author.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Author.aspx.cs" Inherits="BulletinReader.Author" MasterPageFile="~/Layout.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Browse</title>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+    <div class="container">
+        <div class="page-header">
+            <h1>Author <small><%= this.AuthorEntity.Name %></small></h1>
+        </div>
+
+        <asp:Repeater ID="ArticleRepeater" runat="server">
+            <HeaderTemplate>
+                <div class="row">
+            </HeaderTemplate>
+            <ItemTemplate>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <!-- <img src="<%# Eval("Article.CoverImagePath") %>" alt="<%# Eval("Article.Title") %>" /> -->
+                        <div class="caption">
+                            <h3><%# Eval("Article.Title") %></h3>
+                            <p>
+                                <em><%# Eval("Article.Review") %></em></a>
+                            </p>
+                            <div class="pull-right">
+                                <a href="Purchase.aspx?id=<%# Eval("Article.ArticleId") %>" class="btn btn-default btn-sm" role="button">Purchase</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+            <FooterTemplate>
+                </div>
+            </FooterTemplate>
+        </asp:Repeater>
+
+        <ul class="pagination">
+            <asp:Literal ID="ArticlePaging" runat="server" />
+        </ul>
+    </div>
+</asp:Content>
---
+++ BulletinReader/Author.aspx.cs
@@ -0,0 +1,107 @@
+﻿namespace BulletinReader
+{
+    using System;
+    using System.Linq;
+    using BulletinReader.DataClasses;
+    using BulletinReader.Utils;
+    using AuthorDataClass = BulletinReader.DataClasses.Author;
+
+    public partial class Author : BasePage
+    {
+        public const int PageSize = 25;
+
+        public int CurrentPage
+        {
+            get
+            {
+                if (this.Request.QueryString["page"] == null)
+                {
+                    return 1;
+                }
+
+                return int.Parse(this.Request.QueryString["page"]);
+            }
+        }
+
+        public AuthorDataClass AuthorEntity
+        {
+            get;
+            private set;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Guid authorGuid;
+            if (string.IsNullOrWhiteSpace(this.Request.QueryString["id"]) || !Guid.TryParse(this.Request.QueryString["id"], out authorGuid) || authorGuid == Guid.Empty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var authors = (from author in Global.Instance.DbContextMain.Authors
+                           where author.AuthorId == authorGuid
+                           select author);
+
+            this.AuthorEntity = authors.FirstOrDefault();
+
+            if (!this.IsPostBack)
+            {
+                this.FetchData();
+            }
+        }
+
+        private void FetchData()
+        {
+            var articles = (from article in Global.Instance.DbContextMain.Articles.Where(a => a.AuthorId == this.AuthorEntity.AuthorId)
+                            orderby article.StoreDate descending, article.PublishDate descending
+                            select new { Article = article, Author = article.Author });
+
+            int skip = (this.CurrentPage - 1) * Default.PageSize;
+            int rowCount = 0;
+            if (skip >= 0 && this.AuthorEntity != null)
+            {
+                this.ArticleRepeater.DataSource = articles.Skip(skip).Take(Default.PageSize).ToList();
+                this.ArticleRepeater.DataBind();
+
+                rowCount = articles.Count();
+            }
+
+            this.ArticlePaging.Text = "";
+
+            if (this.CurrentPage > 1)
+            {
+                this.ArticlePaging.Text += string.Format("<li><a href=\"Author.aspx?id={0}&page={1}\">&laquo;</a></li>", this.AuthorEntity.AuthorId, this.CurrentPage - 1);
+            }
+            else
+            {
+                this.ArticlePaging.Text += "<li class=\"disabled\"><span>&laquo;</span></li>";
+            }
+
+            int pageCount = rowCount / Default.PageSize;
+            if (pageCount <= 0)
+            {
+                pageCount = 1;
+            }
+
+            for (int i = 1; i <= pageCount; i++)
+            {
+                string addClass = string.Empty;
+
+                if (i == this.CurrentPage)
+                {
+                    addClass = " class=\"active\"";
+                }
+
+                this.ArticlePaging.Text += string.Format("<li{0}><a href=\"Author.aspx?id={1}&page={2}\">{2}</a></li>", addClass, this.AuthorEntity.AuthorId, i);
+            }
+
+            if (this.CurrentPage < pageCount)
+            {
+                this.ArticlePaging.Text += string.Format("<li><a href=\"Author.aspx?id={0}&page={1}\">&raquo;</a></li>", this.AuthorEntity.AuthorId, this.CurrentPage + 1);
+            }
+            else
+            {
+                this.ArticlePaging.Text += "<li class=\"disabled\"><span>&raquo;</span></li>";
+            }
+        }
+    }
+}
